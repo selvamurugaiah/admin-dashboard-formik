da8ec26037fc8c7688da3a6a8c70bb9fb8869f2e
selvamurugaiah <selvamurugaiah100@gmail.com>
2023-03-28 00:41:00 +0530

    Formik-task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Home from "./components/Home";
+import Header from "./components/Header";
+import NotFound from "./components/NotFound";
+import AddMark from "./components/AddMark";
+import EditMark from "./components/EditMark";
+import ViewMark from "./components/ViewMark";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [mode, setMode] = useState("light");
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ThemeProvider theme={darkTheme}>
+        <Paper elevation={3} style={{ minHeight: "100vh" }}>
+          <Header mode={mode} setMode={setMode} />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/add" element={<AddMark />} />
+            <Route path="/student/edit/:id" element={<EditMark />} />
+            <Route path="/student/view/:id" element={<ViewMark />} />
+            <Route path="/404" element={<NotFound />} />
+            <Route path="*" element={<Navigate replace to="/404" />} />
+          </Routes>
+        </Paper>
+      </ThemeProvider>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/api/api.js
@@ -0,0 +1,1 @@
+export const API = "https://624e6fbb77abd9e37c86ffd1.mockapi.io/users";
